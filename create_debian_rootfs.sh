ecce4c59cf0d84d860d54aade0ee7afaffaa5681
taikulawo <qaq1362211689@gmail.com>
2024-05-03 19:54:54 +0800

    1

--- create_debian_rootfs.sh
+++ create_debian_rootfs.sh
@@ -5,7 +5,9 @@ FS=/mnt/ext4
 apt install debootstrap -y
 # create rootfs
 # 1G
-umount $FS
+if [[ "$(mount | grep $FS)" != "" ]]; then
+    umount $FS
+fi
 dd if=/dev/zero of=rootfs.ext4 bs=1M count=1024
 mkfs.ext4 rootfs.ext4
 mkdir -p $FS
