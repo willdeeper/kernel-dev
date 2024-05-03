f821e84772a3d3e690aaf9ca82a5339a365cf48f
taikulawo <qaq1362211689@gmail.com>
2024-05-03 20:21:03 +0800

    1

--- create_debian_rootfs.sh
+++ create_debian_rootfs.sh
@@ -12,6 +12,11 @@ dd if=/dev/zero of=rootfs.ext4 bs=1M count=1024
 mkfs.ext4 rootfs.ext4
 mkdir -p $FS
 mount -o loop rootfs.ext4 $FS
-debootstrap --arch amd64 sid $FS
+debootstrap --arch amd64 sid $FS https://mirrors.tuna.tsinghua.edu.cn/debian
 cp -rf debianrootfs/* $FS
+
+chroot $FS
+# setup in rootfs
+printf "123456\n123456" | passwd root -R /mnt/ext4
+exit
 umount $FS
