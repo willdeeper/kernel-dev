10e9cf75a9a5511c4faaaef38c2835951d16ed42
taikulawo <qaq1362211689@gmail.com>
2024-05-04 00:00:37 +0800

    1

--- debianrootfs/root/rootfs_init.sh
+++ debianrootfs/root/rootfs_init.sh
@@ -4,7 +4,9 @@
 PASSWD=123456
 
 apt update
-apt install wget curl libbpf-dev libssl-dev build-essential clang git bpftool linux-perf pkg-config tcpdump llvm \
-    automake m4 autoconf libpcap-dev openssh-server libelf libc6-dev-i386 -y
+apt install wget curl libbpf-dev libelf-dev libssl-dev build-essential clang git bpftool \
+    linux-perf pkg-config tcpdump llvm automake m4 autoconf libpcap-dev openssh-server \
+    libelf libc6-dev-i386 libxdp-dev vim -y
 echo -e "$PASSWD\n$PASSWD\n" | passwd root
+sysctl -w kernel.printk="2 4 1 7"
 exit
