6c715512d8b82c68bed38cdc99fe15a47e853b10
taikulawo <qaq1362211689@gmail.com>
2024-05-05 14:56:58 +0800

    1

--- debianrootfs/root/rootfs_init.sh
+++ debianrootfs/root/rootfs_init.sh
@@ -8,7 +8,8 @@ echo -e "$PASSWD\n$PASSWD\n" | passwd root
 apt update
 apt install wget curl libbpf-dev libelf-dev libssl-dev build-essential clang git bpftool \
     linux-perf pkg-config tcpdump llvm automake m4 autoconf libpcap-dev openssh-server \
-    libc6-dev-i386 libxdp-dev vim apt-file sudo locales tmux net-tools file netcat-openbsd -y
+    libc6-dev-i386 libxdp-dev vim apt-file sudo locales tmux net-tools file netcat-openbsd \
+    man-db -y
 apt-file update
 # rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
